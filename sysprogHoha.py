5c4bbb077a81fea710de9418847174f638bf20a3
Mochamad Aulia <praditomo@gmail.com>
2018-12-13 22:46:48 +0700

    add webservice hoha

--- sysprogHoha.py
+++ sysprogHoha.py
@@ -31,9 +31,9 @@ while True:
 	sensor = GPIO.input(17)
 	if sensor == 0 and not buka:
 		#pintu buka
-		print("tunggu buka pintu")
+		print("Menunggu buka pintu")
 		sleep(2)
-		print("ganjel pintu")
+		print("Ganjal pintu")
 		GPIO.output(20, 1)
 		GPIO.output(21, 0)
 		buka = True
@@ -43,7 +43,7 @@ while True:
 		
 	elif sensor == 1 and buka and countdown == 0:
 		#pintu tutup
-		print("pintu tutup")
+		print("Pintu tertutup")
 		GPIO.output(20, 0)
 		GPIO.output(21, 1)
 		buka = False
@@ -52,11 +52,11 @@ while True:
 		cleanup()
 	
 	elif sensor == 1 and buka:
-		print("gaada orang")
+		print("Tidak ada orang")
 		countdown-=1
 		sleep(1)
 	
 	elif sensor == 0 and buka:
-		print("ada orang")
+		print("Ada orang")
 		countdown=5
 		sleep(1)
