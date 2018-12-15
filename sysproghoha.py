3a251fa71bda06228f8bdb67b389cd159aaed49f
Mochamad Aulia <praditomo@gmail.com>
2018-12-15 17:32:15 +0700

    add call code and styling

--- sysproghoha.py
+++ sysproghoha.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template
-import RPi.GPIO as GPIO
 from time import sleep
+# import threading
+import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(0)
@@ -10,8 +11,6 @@ GPIO.cleanup()
 #sensor infra red
 GPIO.setup(17, GPIO.IN)
 
-#buat motor
-
 app=Flask(__name__)
 	
 pins = {
@@ -19,68 +18,86 @@ pins = {
 		21 : {'name' : 'GPIO 21', 'state' : 0}
 		#######_________________CAN ADD MORE GPIO HERE___________________#########
 		}
-#setup
 
 def cleanup():
 	for pin in pins:
 		GPIO.output(pin, 0)
-	
+
+@app.after_request
+def mainProgram():
+	buka = False
+	# status = "Belum Dibuka"
+	countdown = 5
+	while True:
+		sensor = GPIO.input(17)
+		if sensor == 0 and not buka:
+			#pintu buka
+			print("Menunggu buka pintu")
+			sleep(2)
+			print("Ganjal pintu")
+			GPIO.output(20, 1)
+			GPIO.output(21, 0)
+			buka = True
+			countdown = 5
+			sleep(1)
+			cleanup()
+			
+		elif sensor == 1 and buka and countdown == 0:
+			#pintu tutup
+			print("Pintu tertutup")
+			GPIO.output(20, 0)
+			GPIO.output(21, 1)
+			buka = False
+			countdown = 5
+			sleep(1)
+			cleanup()
+		
+		elif sensor == 1 and buka:
+			print("Tidak ada orang")
+			countdown-=1
+			sleep(1)
+		
+		elif sensor == 0 and buka:
+			print("Ada orang")
+			countdown=5
+			sleep(1)
+
 @app.route('/')
 def index():
 		templateData = {
-			# 'data' : status
+			# pins
 		}
-		return render_template('index.html', **templateData)	
+		return render_template('index.html', **templateData)
 
+@app.route("/<action>")
+def action(action): 
+	if action == "open":
+		GPIO.output(20, 1)
+		GPIO.output(21, 0)
+		sleep(1)
+		cleanup()
+
+
+	elif action == "close":
+		GPIO.output(20, 0)
+		GPIO.output(21, 1)
+		sleep(1)
+		cleanup()
+
+	templateData = {
+              'pins'    : pins,
+								}
+	return render_template('index.html', **templateData)	
 
 if(__name__=='__main__'):
 	app.run(debug=True,host='0.0.0.0')
 
-	try:
-		while True:
-			sleep(0.5)
-	except KeyboardInterrupt as e:
-		GPIO.cleanup()
+	# try:
+	# 	while True:
+	# 		sleep(0.5)
+	# except KeyboardInterrupt as e:
+	# 	GPIO.cleanup()
 
 for pin in pins:
 	GPIO.setup(pin, GPIO.OUT)
-	GPIO.output(pin, 0)
-	
-buka = False
-status = "belum dibuka"
-countdown = 5
-while True:
-	sensor = GPIO.input(17)
-	if sensor == 0 and not buka:
-		#pintu buka
-		print("Menunggu buka pintu")
-		sleep(2)
-		print("Ganjal pintu")
-		GPIO.output(20, 1)
-		GPIO.output(21, 0)
-		buka = True
-		countdown = 5
-		sleep(1)
-		cleanup()
-		
-	elif sensor == 1 and buka and countdown == 0:
-		#pintu tutup
-		print("Pintu tertutup")
-		GPIO.output(20, 0)
-		GPIO.output(21, 1)
-		buka = False
-		countdown = 5
-		sleep(1)
-		cleanup()
-	
-	elif sensor == 1 and buka:
-		print("Tidak ada orang")
-		countdown-=1
-		sleep(1)
-	
-	elif sensor == 0 and buka:
-		print("Ada orang")
-		countdown=5
-		sleep(1)
-
-		
+	GPIO.output(pin, 0)		
